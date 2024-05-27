e85fd7af7f995072e2cfd8106d6521c47246208f
YasinAHA <yassine.aithad@educa.jcyl.es>
2024-05-27 12:54:11 +0200

    Clase DBConnection

---
+++ src/main/java/edu/alonso/daw/tema9_ejercicio2/Utils/DBConnection.java
@@ -0,0 +1,27 @@
+package edu.alonso.daw.tema9_ejercicio2.Utils;
+
+import java.sql.Connection;
+
+public class DBConnection {
+	
+	private final String DB_NAME = "CURSOS";
+	
+	private final String URL = "jdbc:mysql://localhost:3306/" + DB_NAME;
+	
+	private final String USER = "root";
+	
+	private final String PASS = "";
+	
+	
+	private static DBConnection instance;
+	
+	private Connection conn;
+	
+	private DBConnection() {
+		
+		
+		
+	}
+
+	
+}
